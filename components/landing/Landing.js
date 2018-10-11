8fdd877d8952418b85185d7658c489272ed6ec48
Kostyantyn Sharovarsky <kostya9@gmail.com>
2018-10-12 01:57:03 +0300

    Add settings

--- components/landing/Landing.js
+++ components/landing/Landing.js
@@ -9,19 +9,20 @@ import './Landing.scss'
 
 export default class Landing extends React.Component {
     render() {
-        return <Box align='center' justify='center' alignSelf='center' flex='grow' responsive={true} full='vertical'>
+        return
+        <Box align='center' justify='center' alignSelf='center' flex='grow' responsive={true} full='vertical'>
             <Columns justify='center'>
                 <Box align='center' textAlign='center'>
                     <Card heading="New project" thumbnail="http://icons.iconarchive.com/icons/matiasam/ios7-style/512/Notes-icon.png"
-                    className='notes-card' margin={{horizontal: 'small'}}>
+                        className='notes-card' margin={{ horizontal: 'small' }}>
                     </Card>
                 </Box>
                 <Box align='center' textAlign='center'>
                     <Card heading="from Google Drive" thumbnail="http://icons.iconarchive.com/icons/marcus-roberto/google-play/512/Google-Drive-icon.png"
-                    className='notes-card' margin={{horizontal: 'small'}}>
+                        className='notes-card' margin={{ horizontal: 'small' }}>
                     </Card>
                 </Box>
             </Columns>
-            </Box>
+        </Box>
     }
 } 
